6269490d9ed79b9417a72acfa26edcc8ab569801
Daudi Amani <dsaronin@gmail.com>
2010-12-13 15:28:05 -0800

    0.1-5 logo split to masthead/logo

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Refinery::Application.initialize!
-RefinerySetting.set(:version,'0.1-4')
+RefinerySetting.set(:version,'0.1-5')
