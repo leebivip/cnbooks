7a5663850d3ab9f1804eb19c8167a4a139c4bf7e
Daudi Amani <dsaronin@gmail.com>
2010-12-14 14:08:41 -0800

    0.1-10 dynamic left/right sides

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,28 @@
 module ApplicationHelper
   include Refinery::ApplicationHelper # leave this line in to include all of Refinery's core helper methods.
 
+# -----------------------------------------------------------------------------  
+# choose_body_floats -- dynamically choose float for body content sides of page
+# this allows admin to change refinery setting (:gardenia_main_content_side)
+# to make the sides switch selectable .. but that means we must dynamically 
+# set the css float style for the sections
+# -----------------------------------------------------------------------------  
+  def choose_body_floats( section )
+    main_is_left = (RefinerySetting.get(:gardenia_main_content_side) =~ /left/i)
+    case section
+      when :body_content_title,:body_content_left  then (main_is_left ? 'left' : 'right' )
+      when :body_content_right then  (main_is_left ? 'right' : 'left' )
+    else
+      'right'
+    end  # case
+      
+  end
+# -----------------------------------------------------------------------------
+# select_outer_div -- returns the outer div for properly selecting content sides of page
+# -----------------------------------------------------------------------------  
+  def select_outer_div( section )
+    return "<div id = '#{section == :body_content_left ?  "main_content" : "sidebar_content" }' style = 'float: #{choose_body_floats( section )};'>"    
+  end
+# -----------------------------------------------------------------------------  
+# -----------------------------------------------------------------------------  
 end
--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -6,4 +6,5 @@
   [:gardenia_hf_stretch,false],
   [:gardenia_nav_search,false],
   [:gardenia_side_search,false],
+  [:gardenia_main_content_side,'left'],
 ].each{|setting,default| RefinerySetting.set(setting,default) if RefinerySetting.get(setting).nil?}
