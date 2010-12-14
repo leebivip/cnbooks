8c2cc737dfc1e59753c841b563bec11f60e41f17
Daudi Amani <dsaronin@gmail.com>
2010-12-13 17:36:12 -0800

    0.1-6 header area draft 1

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -3,4 +3,6 @@
 [
   [:version,'0.1-0'],
   [:sf_stretch,false],
+  [:nav_search,true],
+  [:side_search,false],
 ].each{|setting,default| RefinerySetting.set(setting,default) if RefinerySetting.get(setting).nil?}
