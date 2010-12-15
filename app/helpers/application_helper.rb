1df4548d76de796f4c0645fe498e8b3619fc89a2
Daudi Amani <dsaronin@gmail.com>
2010-12-15 00:58:06 -0800

    0.1-13 content_page/application_helper.rb altered to support sidebar boxes via refinery page parts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,6 +28,23 @@ module ApplicationHelper
     return "<div id = '#{section == :body_content_left ?  "main_content" : "sidebar_content" }' style = 'float: #{choose_body_floats( section )};'>"    
   end
 
+# -----------------------------------------------------------------------------
+# prep_dom_section -- prepatory stuff for an individual section; returns dom_id
+# preps the fall back stuff
+# ----------------------------------------------------------------------------- 
+  def prep_dom_section(section, need_fallback )
+    section[:html] = ( content_for( section[:yield] ) )
+    
+    if section[:html].blank? and need_fallback and
+        section.keys.include?(:fallback) and
+        section[:fallback].present?
+        
+      section[:html] = section[:fallback].to_s.html_safe
+      
+    end  # if
+
+    return ( section[:id] ||= section[:yield].to_s )  # dom_id
+  end
 # -----------------------------------------------------------------------------  
 # prepare_content_page -- replaces *ALL* the erb/haml code on shared/content_page
 # args: assumed to be local variables prior to invoking the view?
@@ -38,44 +55,50 @@ module ApplicationHelper
 #   @class_list -- list of css classes required
 # -----------------------------------------------------------------------------  
   def prepare_content_page(show_empty_sections, remove_automatic_sections , hide_sections)
+  
+    need_fallback = (!show_empty_sections and !remove_automatic_sections)
     
     @sections = [
       {:yield => :body_content_title, :fallback => page_title, :id => 'body_content_page_title', :title => true},
       {:yield => :body_content_right, :fallback => (@page.present? ? @page[Page.default_parts.second.to_sym] : nil)},
-    ]
+      {:yield => :body_content_left, :fallback => (@page.present? ? @page[Page.default_parts.first.to_sym] : nil)}
+    ].reject{ |section| hide_sections.include?( section[:yield] ) }
     
-#    if @page.present? && Page.default_parts.size > 2  # need any sidebar boxes?
-#      Page.default_parts[2,Page.default_parts.size - 2].each do |part|
-#      @sections <<  {:yield => part.to_sym, :fallback => part.to_sym}
-#    end
+    if @page.present? && Page.default_parts.size > 2  # need any sidebar boxes?
+      
+      sidebar_sections = Page.default_parts[2,Page.default_parts.size - 2].inject([]) do |list, part|
+        list << {:yield => part.to_sym, :fallback => part.to_sym}
+      end  # do each page part
+        
+      sidebar_sections.reject{ |section| hide_sections.include?( section[:yield] ) }
+          
+    end
     
-    @sections <<  {:yield => :body_content_left, :fallback => (@page.present? ? @page[Page.default_parts.first.to_sym] : nil)}
-    
-    @sections.reject{ |section| hide_sections.include?( section[:yield] ) }
-  
     css = []
   
     @sections.each do |section|
-      dom_id = ( section[:id] ||= section[:yield].to_s )
-      section[:html] = ( content_for( section[:yield] ) )
-      
-      if section[:html].blank? and
-          !show_empty_sections and
-          !remove_automatic_sections and
-          section.keys.include?(:fallback) and
-          section[:fallback].present?
-          
-        section[:html] = section[:fallback].to_s.html_safe
-        
-      end  # if
+
+      dom_id = prep_dom_section(section, need_fallback )
       
       unless section[:html].blank?
       # see application_helper.rb for select_outer_div and choose_body_floats
     
-        unless section[:title]
-          section[:html] = "#{select_outer_div( section[:yield] )} <div class='clearfix' id='#{dom_id}'>#{section[:html]} </div> </div>"
-        else
+        if section[:title]
           section[:html] = "<div id = 'body_content_title' style = 'float: #{choose_body_floats( section[:yield] )};'> <h1 id='#{dom_id}'>#{section[:html]}</h1> </div>"
+        else
+          section[:html] = "#{select_outer_div( section[:yield] )} <div class='clearfix' id='#{dom_id}'>#{section[:html]} </div> "
+          
+            # if this is the sidebar area, then also insert subsidiary boxes to sidebar
+          if section[:yield] == :body_content_right
+            sidebar_sections.each do |sub_section|
+              sub_dom_id = prep_dom_section(sub_section, need_fallback )
+              unless sub_section[:html].blank?
+                section[:html] << "<div class='sidebar_box clearfix' id='#{sub_dom_id}'>#{sub_section[:html]} </div> "
+              end  # sub_section unless
+            end  # each sub_section
+          end # special sidebar handling
+          
+          section[:html] << "</div>" # attach trailing div closure
         end  # innermost unless .. else
         
       else
@@ -87,7 +110,6 @@ module ApplicationHelper
     @class_list = "clearfix#{" #{css.join(' ')}" if css.any?}"
 
   end
-
 
 
 # -----------------------------------------------------------------------------  
