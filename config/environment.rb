8de99424c26b52fc0d9d21b8b1df6024879a43e7
Daudi Amani <dsaronin@gmail.com>
2010-12-22 11:53:05 -0800

    0.1-29 rightbar h defs in sass

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Refinery::Application.initialize!
-RefinerySetting.set(:gardenia_version,'0.1-28')
+RefinerySetting.set(:gardenia_version,'0.1-29')
