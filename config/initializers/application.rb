4d90812e4209b697ed52b22cd45ee81854e0a980
Daudi Amani <dsaronin@gmail.com>
2010-12-27 21:05:31 -0800

    0.1-37 init/app.rb/env.rb conditionally check RefinerySetting.table_exists?

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,12 +1,14 @@
 # initialize all gardenia-expected settings
 # format is: [variable, default]
-[
-  [:site_name, 'Gardenia'],
-  [:gardenia_version,'0.1-0'],
-  [:gardenia_hf_stretch,false],
-  [:gardenia_nav_search,false],
-  [:gardenia_side_search,false],
-  [:gardenia_main_content_side,'left'],
-  [:gardenia_parts_allow_widgets, %w(Widgets)],
-  [:gardenia_disable_repeat_title, true],
-].each{|setting,default| RefinerySetting.set(setting,default) if RefinerySetting.get(setting).nil?}
+ if RefinerySetting.table_exists?
+  [
+    [:site_name, 'Gardenia'],
+    [:gardenia_version,'0.1-0'],
+    [:gardenia_hf_stretch,false],
+    [:gardenia_nav_search,false],
+    [:gardenia_side_search,false],
+    [:gardenia_main_content_side,'left'],
+    [:gardenia_parts_allow_widgets, %w(Widgets)],
+    [:gardenia_disable_repeat_title, false],
+  ].each{|setting,default| RefinerySetting.set(setting,default) if RefinerySetting.get(setting).nil?}
+ end
