a7329e18d39c5acbc9f1b6fba8eda4deb81d91a0
Daudi Amani <dsaronin@gmail.com>
2011-01-07 14:47:01 -0800

    0.2-1 basic default gardenia post jazz

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Refinery::Application.initialize!
-RefinerySetting.set(:gardenia_version,'0.1-46') if RefinerySetting.table_exists?
+RefinerySetting.set(:gardenia_version,'0.2-1') if RefinerySetting.table_exists?
