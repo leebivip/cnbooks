eca97381a62ff649e5b170bdf4a0d1c23e112ba8
Alexis Masters <alexismasters@gmail.com>
2011-06-24 09:01:12 -0700

    cnbooks css tweaks

---
+++ app/models/news_item.rb
@@ -0,0 +1,44 @@
+class NewsItem < ActiveRecord::Base
+  belongs_to :image, :class_name => 'Image'
+
+  translates :title, :body, :external_url
+  
+  attr_accessor :locale # to hold temporarily
+
+  alias_attribute :content, :body
+  validates_presence_of :title, :content, :publish_date
+
+  has_friendly_id :title, :use_slug => true
+
+  acts_as_indexed :fields => [:title, :body]
+
+  default_scope :order => "publish_date DESC"
+
+  # If you're using a named scope that includes a changing variable you need to wrap it in a lambda
+  # This avoids the query being cached thus becoming unaffected by changes (i.e. Time.now is constant)
+  scope :published, lambda {
+    where( "publish_date < ?", Time.now )
+  }
+  scope :latest, lambda { |*l_params|
+    published.limit( l_params.first || 10)
+  }
+
+  # rejects any page that has not been translated to the current locale.
+  scope :translated, lambda {
+    pages = Arel::Table.new(NewsItem.table_name)
+    translations = Arel::Table.new(NewsItem.translations_table_name)
+
+    includes(:translations).where(
+      translations[:locale].eq(Globalize.locale)).where(pages[:id].eq(translations[:news_item_id]))
+  }
+
+  def not_published? # has the published date not yet arrived?
+    publish_date > Time.now
+  end
+
+  # for will_paginate
+  def self.per_page
+    20
+  end
+
+end
