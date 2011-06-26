07a4497ffd3f44236e3bc2ab4dfab7f6e30d8e9e
Daudi Amani <dsaronin@gmail.com>
2011-06-26 00:22:58 -0700

    transtion to ref1.0.3

--- erb2haml.rb
+++ erb2haml.rb
@@ -1,16 +1,31 @@
 #!/usr/bin/env ruby
 
 class ToHaml
-  def initialize(path)
-    @path = path
+  def initialize(srcpath,dstpath)
+    @srcpath = srcpath
+    @dstpath = dstpath
   end
   
   def convert!
-    Dir["#{@path}/**/*.erb"].each do |file|
-      `html2haml -rx #{file} #{file.gsub(/\.erb$/, '.haml')}`
+    Dir["#{@srcpath}/*.erb"].each do |file|
+      dstfile = file.sub( Regexp.new( @srcpath ), @dstpath).sub(/\.erb$/, '.haml')
+      puts "---- converting #{file} to #{dstfile}"
+      `html2haml -rx #{file} #{dstfile}`
     end
   end
 end
 
-path = File.join(File.dirname(__FILE__), 'app', 'views')
-ToHaml.new(path).convert!
+  # first parameter can either be a subdirectory in views, a full path, or nothing
+if ARGV[0] && ARGV[0] =~ Regexp.new("^/")
+  srcpath = ARGV[0]
+else
+  view = ARGV[0] || "/**"
+  srcpath = File.join(File.dirname(__FILE__), 'app', 'views', view)
+end
+
+  # second parameter is destination directory path
+dstpath = File.join(File.dirname(__FILE__), (ARGV[1] || "tmp/haml") )
+
+puts "** Starting erb2haml source: #{srcpath}, dest: #{dstpath}"
+
+ToHaml.new(srcpath, dstpath).convert!
