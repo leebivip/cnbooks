5770af52714d2f2bbb455282b9673fef97905930
Daudi Amani <dsaronin@gmail.com>
2011-07-01 23:23:40 -0700

    refcms1.0.3 some progress - 1

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gardenia::Application.config.secret_token = '88fc4a1a936161a3f3f827c67219a823672bff90e73087c02c40e6ea14bebf5bc4cc77bd3c34493c8942329656bac5929e92f2166df14fa4913c4e56336eced8'
+Gardenia::Application.config.secret_token = '88fc4a1aa46161a3f3f827c67219a823672bff90e73087c02c40e6ea14bebf5bc4cc77bd3c34493c8942329656bac5929e92f2166df14fa4913c4e56336eced8'
