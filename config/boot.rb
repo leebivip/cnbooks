4bcd6b2943058a1d9dd92af72077e00eea55055b
Daudi Amani <dsaronin@gmail.com>
2011-07-05 17:03:21 -0700

    still migrating gardenia to refcms 1.0.3

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,12 @@
 require 'rubygems'
 
+# next ines needed to fix following exception when running under ruby 1.9.2
+# couldn't parse YAML at line 7 column 0 (Psych::SyntaxError)
+unless RUBY_VERSION =~ /1\.8\.7/
+  require 'yaml'
+  YAML::ENGINE.yamler= 'syck'
+end
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
