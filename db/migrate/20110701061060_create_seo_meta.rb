0819bb07ca770000ae4581e7fa21a88c3041425a
Daudi Amani <dsaronin@gmail.com>
2011-07-14 13:04:45 -0700

    aligned 1.0.3 migrations with latest from github source, but removed ::Refinery

--- db/migrate/20110701061060_create_seo_meta.rb
+++ db/migrate/20110701061060_create_seo_meta.rb
@@ -16,26 +16,26 @@ class CreateSeoMeta < ActiveRecord::Migration
     add_index :seo_meta, [:seo_meta_id, :seo_meta_type]
 
     # Grab the attributes of the records that currently exist
-    existing_translations = ::Refinery::Page.translation_class.all.map(&:attributes)
+    existing_translations = ::Page.translation_class.all.map(&:attributes)
 
     # Remove columns
     ::SeoMeta.attributes.keys.each do |field|
-      if ::Refinery::Page.translation_class.column_names.map(&:to_sym).include?(field)
-        remove_column ::Refinery::Page.translation_class.table_name, field
+      if ::Page.translation_class.column_names.map(&:to_sym).include?(field)
+        remove_column ::Page.translation_class.table_name, field
       end
     end
 
     # Reset column information because otherwise the old columns will still exist.
-    ::Refinery::Page.translation_class.reset_column_information
+    ::Page.translation_class.reset_column_information
 
     # Re-attach seo_meta
-    ::Refinery::Page.translation_class.module_eval do
+    ::Page.translation_class.module_eval do
       is_seo_meta
     end
 
     # Migrate data
     existing_translations.each do |translation|
-      ::Refinery::Page.translation_class.find(translation['id']).update_attributes(
+      ::Page.translation_class.find(translation['id']).update_attributes(
         ::SeoMeta.attributes.keys.inject({}) {|attributes, name|
           attributes.merge(name => translation[name.to_s])
         }
@@ -43,34 +43,34 @@ class CreateSeoMeta < ActiveRecord::Migration
     end
 
     # Reset column information again because otherwise the old columns will still exist.
-    ::Refinery::Page.reset_column_information
+    ::Page.reset_column_information
   end
 
   def self.down
     # Grab the attributes of the records that currently exist
-    existing_translations = ::Refinery::Page.translation_class.all.map(&:attributes)
+    existing_translations = ::Page.translation_class.all.map(&:attributes)
 
     # Add columns back to your model
-    ::Refinery::SeoMeta.attributes.each do |field, field_type|
-      unless ::Refinery::Page.translation_class.column_names.map(&:to_sym).include?(field)
-        add_column ::Refinery::Page.translation_class.table_name, field, field_type
+    ::SeoMeta.attributes.each do |field, field_type|
+      unless ::Page.translation_class.column_names.map(&:to_sym).include?(field)
+        add_column ::Page.translation_class.table_name, field, field_type
       end
     end
 
     # Reset column information because otherwise the new columns won't exist yet.
-    ::Refinery::Page.translation_class.reset_column_information
+    ::Page.translation_class.reset_column_information
 
     # Migrate data
     existing_translations.each do |translation|
-      ::Refinery::Page.translation_class.update_all(
-        ::Refinery::SeoMeta.attributes.keys.inject({}) {|attributes, name|
+      ::Page.translation_class.update_all(
+        ::SeoMeta.attributes.keys.inject({}) {|attributes, name|
           attributes.merge(name => translation[name.to_s])
         }, :id => translation['id']
       )
     end
 
-    ::Refinery::SeoMeta.attributes.keys.each do |k|
-      ::Refinery::Page.translation_class.module_eval %{
+    ::SeoMeta.attributes.keys.each do |k|
+      ::Page.translation_class.module_eval %{
         def #{k}
         end
 
@@ -80,7 +80,7 @@ class CreateSeoMeta < ActiveRecord::Migration
     end
 
     # Reset column information again because otherwise the old columns will still exist.
-    ::Refinery::Page.reset_column_information
+    ::Page.reset_column_information
 
     drop_table :seo_meta
   end
