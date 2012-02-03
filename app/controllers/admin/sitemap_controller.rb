7e777cfb86df8b610bce79327832bbac000d9115
Daudi Amani <dsaronin@gmail.com>
2012-02-03 13:10:15 -0800

    added sitemap controller

---
+++ app/controllers/admin/sitemap_controller.rb
@@ -0,0 +1,20 @@
+class SitemapController < ::Refinery::FastController
+  layout nil
+
+  def index
+    @locales = if ::Refinery.i18n_enabled?
+                 ::Refinery::I18n.frontend_locales
+               else
+                 [::I18n.locale]
+               end
+
+    respond_to do |format|
+
+      format.xml do
+        headers['Content-Type'] = 'application/xml'
+      end  # xml format
+
+    end  # all formats
+  end
+
+end
